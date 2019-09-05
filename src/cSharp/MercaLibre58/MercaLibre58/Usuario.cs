cbfc21729922ad142fc8983b21217f8d813660ea
angelesmejias <50706200+angelesmejias@users.noreply.github.com>
2019-09-05 10:01:59 -0300

    Data Annotatios

--- src/cSharp/MercaLibre58/MercaLibre58/Usuario.cs
+++ src/cSharp/MercaLibre58/MercaLibre58/Usuario.cs
@@ -1,10 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace MercaLibre58
-{
+{  
+    [Table ("Usuario")]
     class Usuario
     {
+        [Key]
+        [Column ("IdUsuario"),Required,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdUsuario { get; set; }
+        [Column("Nombre"),Required,StringLength(45)]
+        public string Nombre { get; set; }
+        [Column("Apellido"),Required,StringLength(45)]
+        public string Apellido { get; set; }
+        [Column("Teléfono")]
+        public int Telefono { get; set; }
+        [Column("Nombre_Usuario"),Required,StringLength(100)]
+        public string NombreUsuario { get; set; }
+        [Column("Contraseña_Usuario"),Required,]
+        public string ContraseñaUsuario { get; set; }
+        [Column("Email"),Required,StringLength(100)]
+        public string Email { get; set; }
+       
+        
     }
 }
