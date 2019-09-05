4ddc6e215356969a1b0e5db04179ebace9b8b2a2
Diegoaguilar12 <diego.aguilar.et12@gmail.com>
2019-09-05 10:19:14 -0300

    public clases

--- src/cSharp/MercaLibre58/MercaLibre58/Abstract.cs
+++ src/cSharp/MercaLibre58/MercaLibre58/Abstract.cs
@@ -4,8 +4,13 @@ using System.Text;
 
 namespace MercaLibre58
 {
-    public abstract class Abstract
+    public abstract class EnteConCompras
     {
+        public EnteConCompras()
+        {
+
+        }
+        public List<Compra>Compras { get; set; }
         public void ListaCompras()
         {
             
