5aaa257bfa61d24d633bef58896b52b30be0367e
erilueh <37271713+erilueh@users.noreply.github.com>
2019-09-06 11:16:04 -0300

    Erik 75% Hecho

--- src/cSharp/MercaLibre58/MercaLibre58/Producto.cs
+++ src/cSharp/MercaLibre58/MercaLibre58/Producto.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
 
 namespace MercaLibre58
 {
@@ -10,20 +11,36 @@ namespace MercaLibre58
     public class Producto:EnteConCompras
     {
         [Key,Column("idProducto"),DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int idProducto { get; set; }
+        public int IdProducto { get; set; }
         [Column("cantidad"),Required]
-        public int cantidad { get; set; }
+        public int Cantidad { get; set; }
         [Column("nombreProducto"),Required,StringLength(45)]
-        public string nombreProducto { get; set; }
+        public string NombreProducto { get; set; }
         [Column("precio"),Required]
-        public float precioProducto { get; set; }
-        [ForeignKey("Vendedor")]
-        [Column("vendedor"),Required]
+        public float PrecioProducto { get; set; }
+        [ForeignKey("Vendedor"),Required]
         public Usuario Vendedor { get; set; }
         public Producto() : base()
         {
                
         }
-        
+
+        public void DecrementarCant( int cant)
+        {
+            Cantidad -= cant;
+        }
+
+        public bool StockSuf(int cant) => cant <= this.Cantidad;
+
+        public float RecaudacionPara(DateTime inicio, DateTime fin)
+        {
+            return comprasVentasEntre(inicio, fin).Sum(h => h.Precio);
+        }
+
+        private List<CompraVenta> comprasVentasEntre(DateTime inicio, DateTime fin)
+        {
+            return CompraVentas.FindAll(r => r.Entre(inicio, fin));
+        }
+
     }
 }
--- src/cSharp/MercaLibre58/MercaLibre58/Usuario.cs
+++ src/cSharp/MercaLibre58/MercaLibre58/Usuario.cs
@@ -1,42 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-
+using System.Linq;
 
 namespace MercaLibre58
-{  
-    [Table ("Usuario")]
-    public class Usuario:EnteConCompras
+{
+    [Table("Usuario")]
+    public class Usuario : EnteConCompras
     {
         [Key]
-        [Column ("IdUsuario"),Required,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("IdUsuario"), Required, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int IdUsuario { get; set; }
-        [Column("Nombre"),Required,StringLength(45)]
+        [Column("Nombre"), Required, StringLength(45)]
         public string Nombre { get; set; }
-        [Column("Apellido"),Required,StringLength(45)]
+        [Column("Apellido"), Required, StringLength(45)]
         public string Apellido { get; set; }
         [Column("Teléfono")]
         public int Telefono { get; set; }
-        [Column("Nombre_Usuario"),Required,StringLength(100)]
+        [Column("Nombre_Usuario"), Required, StringLength(100)]
         public string NombreUsuario { get; set; }
-        [Column("Contraseña_Usuario"),Required,]
+        [Column("Contraseña_Usuario"), Required,]
         public string ContraseñaUsuario { get; set; }
-        [Column("Email"),Required,StringLength(100)]
+        [Column("Email"), Required, StringLength(100)]
         public string Email { get; set; }
-        public Usuario():base()
+        public Usuario() : base()
         {
 
-        }   
+        }
         public void Comprar(Producto producto, int cant)
         {
-            CompraVenta compra = new CompraVenta(cant, this, producto);
-            
-            agregarCompra(compra);
-            producto.cantidad -= cant;
-            
+            if (producto.StockSuficiente(cant))
+            {
+                CompraVenta compra = new CompraVenta(cant, this, producto);
+
+                AgregarCompra(compra);
+                producto.DecrementarCant(cant);
+            }
         }
+        public float RecaudacionTotal(DateTime inicio, DateTime fin)
+        {
+            return  FindAll(rt => rt.Entre(inicio, fin)).Sum(rct => rct.TotalPrecio);
+        }
+        /*public float precioPromedioEntre(DateTime inicio, DateTime fin)
+        {
+            //Paso 1: filtro los Historiales que se encuentren entre esas fechas con el FindAll
+            //Paso 2: sobre la lista filtrada, aplico el metodo para conocer el promedio
+            return HistorialPrecios.FindAll(h => h.entre(inicio, fin)).
+                                     Average(h => h.PrecioUnitario);
+        }*/
 
 
     }
