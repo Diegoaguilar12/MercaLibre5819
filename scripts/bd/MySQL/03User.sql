51de7c1beec29f2a87ec60b02549cc5cea43fa25
Diegoaguilar12 <diego.aguilar.et12@gmail.com>
2019-11-21 12:30:51 -0300

    permisos correctos

--- scripts/bd/MySQL/03User.sql
+++ scripts/bd/MySQL/03User.sql
@@ -1,8 +1,8 @@
-create user 'userLibre'@'%' identified by 'contrauser';
-create user 'admin'@'10.120.0.%' identified by 'contraadmin';
+CREATE USER IF NOT EXISTS 'userLibre'@'%' identified by 'contrauser';
+CREATE USER IF NOT EXISTS 'admin'@'10.120.0.%' identified by 'contraadmin';
 
-Grant select, update(Teléfono, Pass, Email) on Usuario to 'userLibre'@'%';
-Grant select,update(precio, Cantidad) on Producto to 'userLibre'@'%';
+Grant insert,select,update(Teléfono, Pass, Email) on Usuario to 'userLibre'@'%';
+Grant insert,select,update(precio, Cantidad) on Producto to 'userLibre'@'%';
 Grant select,update(Teléfono, Pass, Email) on Usuario to 'admin'@'10.120.0.%';
 Grant select on CompraVenta to 'userLibre'@'%';
 Grant Delete on CompraVenta to 'admin'@'10.120.0.%';
