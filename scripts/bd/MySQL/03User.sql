13fb01a7f18484574f66025a6f05b37c66f19465
Diegoaguilar12 <diego.aguilar.12@gmail.com>
2019-11-28 12:05:18 -0300

    Update 03User.sql

--- scripts/bd/MySQL/03User.sql
+++ scripts/bd/MySQL/03User.sql
@@ -1,9 +1,9 @@
-CREATE USER IF NOT EXISTS 'userLibre'@'10.120.0.%' identified by 'contrauser';
+CREATE USER IF NOT EXISTS 'userLibre'@'localhost' identified by 'contrauser';
 CREATE USER IF NOT EXISTS 'admin'@'10.120.0.%' identified by 'contraadmin';
 
 
-Grant insert,select,update(Teléfono, Pass, Email) on Usuario to 'userLibre'@'10.120.0.%';
-Grant insert,select,update(precio, Cantidad) on Producto to 'userLibre'@'10.120.0.%';
+Grant insert,select,update(Teléfono, Pass, Email) on Usuario to 'userLibre'@'localhost';
+Grant insert,select,update(precio, Cantidad) on Producto to 'userLibre'@'localhost';
 Grant select,update(Teléfono, Pass, Email) on Usuario to 'admin'@'10.120.0.%';
-Grant select on CompraVenta to 'userLibre'@'10.120.0.%';
+Grant select on CompraVenta to 'userLibre'@'localhost';
 Grant Delete on CompraVenta to 'admin'@'10.120.0.%';
